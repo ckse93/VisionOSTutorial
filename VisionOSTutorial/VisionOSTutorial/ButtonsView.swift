b55421c9d2617f2d43ec40d07d422f1ccf68b7f5
Chan Jung <chanjung@Zack-MBP-14.local>
2024-03-11 22:01:20 -0500

    ButtonStyle video material

--- VisionOSTutorial/VisionOSTutorial/ButtonsView.swift
+++ VisionOSTutorial/VisionOSTutorial/ButtonsView.swift
@@ -10,41 +10,170 @@ import SwiftUI
 struct ButtonsView: View {
     var body: some View {
         HStack {
+            ButtonsViewDefault()
+            
             Divider()
+            
+            ButtonsViewTypical()
+            
+            Divider()
+            
+            ButtonsViewButtonStyle()
         }
+        .glassBackgroundEffect()
     }
 }
 
-struct ButtonsViewTypicaliOS: View {
+struct ButtonsViewDefault: View {
     var body: some View {
         VStack {
+            Text("default button")
+                .font(.largeTitle)
+                .padding()
+            
             Button {
                 print("button")
             } label: {
                 Text("default button")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             }
-            
-            Button {
-                print("button")
-            } label: {
-                Text("plain button")
-            }
-            .buttonStyle(.plain)
-            
-            Button {
-                print("button")
-            } label: {
-                Text("bordered button")
-            }
-            .buttonStyle(.bordered)
-            
-            Button {
-                print("button")
-            } label: {
-                Text("bordered prominent button")
-            }
-            .buttonStyle(.borderedProminent)
         }
+    }
+}
+
+struct ButtonsViewTypical: View {
+    var body: some View {
+        VStack {
+            Text("typical approach")
+                .font(.largeTitle)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("iOS pattern custom capsule button")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical)
+                    .padding()
+                    .background(Color(uiColor: .systemOrange))
+                    .clipShape(Capsule())
+            })
+            .padding()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("iOS pattern custom rectangle button")
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding()
+                    .background(Color(uiColor: .systemOrange))
+                    .clipShape(
+                        RoundedRectangle(cornerRadius: 15.0)
+                    )
+            })
+            .padding()
+        }
+        
+    }
+}
+
+struct ButtonsViewButtonStyle: View {
+    var body: some View {
+        VStack {
+            Text("using buttonStyle")
+                .font(.largeTitle)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("custom ButtonStyle")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical)
+                    .padding()
+                    .background(Color(uiColor: .systemOrange))
+                    .clipShape(Capsule())
+            })
+            .buttonStyle(.custom)
+            .padding()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("iOS pattern custom rounded rectangle button")
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding()
+                    .background(Color(uiColor: .systemOrange))
+            })
+            .buttonStyle(.rounded)
+            .padding()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("iOS pattern custom rectangle button")
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding()
+                    .background(Color(uiColor: .systemOrange))
+            })
+            .buttonStyle(.rectangular)
+            .padding()
+
+        }
+    }
+}
+
+struct RecButtonStyle: PrimitiveButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: configuration.trigger, label: {
+            configuration.label
+                .hoverEffect()
+        })
+        .buttonStyle(.plain)
+    }
+}
+
+extension PrimitiveButtonStyle where Self == RecButtonStyle {
+    static var rectangular: Self {
+        .init()
+    }
+}
+
+struct RoundedButtonStyle: PrimitiveButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: configuration.trigger, label: {
+            configuration.label
+                .hoverEffect()
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 15)
+                )
+        })
+        .buttonStyle(.plain)
+        .clipShape(
+            RoundedRectangle(cornerRadius: 15)
+        )
+    }
+}
+
+extension PrimitiveButtonStyle where Self == RoundedButtonStyle {
+    static var rounded: Self {
+        .init()
+    }
+}
+
+struct CustomButtonStyle: PrimitiveButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: configuration.trigger, label: {
+            configuration.label
+        })
+        .buttonStyle(.plain)
+    }
+}
+
+extension PrimitiveButtonStyle where Self == CustomButtonStyle {
+    static var custom: Self {
+        .init()
     }
 }
 
